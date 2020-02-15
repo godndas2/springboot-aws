b7ad97aa5fe303401290640d9942ca698bc29205
halfdev <gusgodnd@naver.com>
2020-02-15 23:49:21 +0900

    re upload

---
+++ src/main/java/org/sb/aws/rest/PostsApiController.java
@@ -0,0 +1,45 @@
+package org.sb.aws.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.sb.aws.rest.dto.PostsListResponseDto;
+import org.sb.aws.rest.dto.PostsResponseDto;
+import org.sb.aws.rest.dto.PostsSaveRequestDto;
+import org.sb.aws.rest.dto.PostsUpdateRequestDto;
+import org.sb.aws.service.PostsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+}
+
---
+++ src/main/java/org/sb/aws/service/PostsService.java
@@ -0,0 +1,58 @@
+package org.sb.aws.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sb.aws.entity.posts.Posts;
+import org.sb.aws.entity.posts.PostsRepository;
+import org.sb.aws.rest.dto.PostsListResponseDto;
+import org.sb.aws.rest.dto.PostsResponseDto;
+import org.sb.aws.rest.dto.PostsSaveRequestDto;
+import org.sb.aws.rest.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
