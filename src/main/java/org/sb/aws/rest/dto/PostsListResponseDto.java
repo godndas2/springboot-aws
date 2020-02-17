a811518161ef16a77c26b4b99cdb29dc5841f2f8
halfdev <gusgodnd@naver.com>
2020-02-17 23:05:35 +0900

    List dateformat

--- src/main/java/org/sb/aws/rest/dto/PostsListResponseDto.java
+++ src/main/java/org/sb/aws/rest/dto/PostsListResponseDto.java
@@ -4,19 +4,22 @@ import lombok.Getter;
 import org.sb.aws.entity.posts.Posts;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Getter
 public class PostsListResponseDto {
+
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime modifiedDate;
+    private String modifiedDate; //
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.modifiedDate = entity.getModifiedDate();
+        this.modifiedDate = entity.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 }
 
