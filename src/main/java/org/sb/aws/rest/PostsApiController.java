d24c8c36b8baf17192cf5537eb2f2bc93461697d
halfdev <gusgodnd@naver.com>
2020-02-20 22:47:53 +0900

    oauth redirect 404 resolve

--- src/main/java/org/sb/aws/rest/PostsApiController.java
+++ src/main/java/org/sb/aws/rest/PostsApiController.java
@@ -22,6 +22,7 @@ public class PostsApiController {
     }
 
     @PutMapping("/api/v1/posts/{id}")
+
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
